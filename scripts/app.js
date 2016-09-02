5bd0e0184ccd631855069ab6bdd7b6e65245191f
oskaryil <15bayi@stockholmscience.se>
2016-09-02 09:45:17 +0200

    .

--- scripts/app.js
+++ scripts/app.js
@@ -51,7 +51,7 @@ render: function() {
         <h1 className="text-center heading">To-Do List</h1>
         <TodoListItems data={this.state.data} />
         <TodoListForm onItemSubmit={this.handleItemSubmit} />
-        <button className="btn btn-danger">Clear All</button>
+        <button className="btn btn-danger" onClick={this.clearLocalStorage()}>Clear All</button>
       </div>
   );
 }
